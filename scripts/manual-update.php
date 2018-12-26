609e48b32d506a1ab4a14f551d4aa840f349c43c
Kayla Colflesh <kcolflesh97@gmail.com>
2018-12-25 20:18:18 -0500

    Update manual-update.php

--- scripts/manual-update.php
+++ scripts/manual-update.php
@@ -1,59 +1,57 @@
-#!/usr/bin/php
 <?php
+function process($manager, $key, $logger) {
+  if (!$manager->compareWebAccessKeys($key)) {
+    $logger->info('User provided the wrong key : key = ' . $key);
+    return ['message' => 'Invalid key', 'code' => 1];
+  }
 
-if (!isset($argv)) {
-  echo 'Access denied', PHP_EOL;
-  return;
+  if (!isset($_GET['id'])) {
+    $logger->info('User failed to provide a repository id');
+    return ['message' => 'No repository specified!', 'code' => 1];
+  }
+
+  $id = trim($_GET['id']);
+  $repo = $manager->getRepositoryById($id);
+
+  if ($repo === null) {
+    $logger->info('User provided a non-existent repository id: ' . $id);
+    return ['message' => 'Unable to find repository!', 'code' => 1];
+  }
+
+  $logger->info('User started update of repository: ' . $repo);
+  try {
+    $manager->initalize();
+    $manager->execute($repo);
+  } catch (Exception $e) {
+    $logger->error($e);
+    $manager->cleanUp();
+    return ['message' => 'Exception! ' . $e, 'code' => 1];
+  }
+  $manager->cleanUp();
+
+  $logger->info('Successfully updated repository: ' . $repo);
+  return ['message' => 'Finished update!', 'code' => 0];
 }
 
-if (count($argv) < 2) {
-  echo 'You must pass the ids of the repositories as an argument!', PHP_EOL;
-  return;
-}
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+date_default_timezone_set('America/New_York');
+
+$projectDirectory = dirname(__DIR__);
+require $projectDirectory . '/vendor/autoload.php';
 
 use AutomatedGitDeployer\Config;
+use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogHandler;
-use Monolog\Logger;
 
+$key     = isset($_GET['key']) ? $_GET['key'] : null;
 $manager = Config::createGitHookManagerFromDefaults();
 $logger  = new Logger('automated_git_deployer');
-$logger->pushHandler(new StreamHandler('php://stdout'));
+
+$logger->pushHandler(new SyslogHandler('php_automated_git_deployer'));
 $manager->logger = $logger;
 
-$repositories = [];
+$result = process($manager, $key, $logger);
 
-array_shift($argv);
-
-$usedWildcard = false;
-if (count($argv) === 1) {
-  $firstArgument = $argv[0];
-  if ('*' === $firstArgument) {
-    $repositories = $manager->getAllRepositories();
-    $usedWildcard = true;
-  }
-}
-
-if (!$usedWildcard) {
-  foreach ($argv as $argument) {
-    $repository = $manager->getRepositoryById($argument);
-    if (null === $repository) {
-      $logger->error('The following repository does not exist : ' . $argument);
-      return;
-    }
-    $repositories[] = $repository;
-  }
-}
-
-try {
-  $manager->initalize();
-
-  foreach ($repositories as $repository) {
-    $manager->execute($repository);
-  }
-} catch (Exception $e) {
-  $logger->error($e);
-}
-$manager->cleanUp();
+header('Content-type: application/json');
+echo json_encode($result);
